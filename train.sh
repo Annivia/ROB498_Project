1ca79b91c8f0c8edc8c5ada05c3265ca46cfc3e1
Pony Xi <ponyz@umich.edu>
2023-04-14 23:37:01 -0400

    Update training script

--- train.sh
+++ train.sh
@@ -1,3 +1,15 @@
-python train.py --train --algorithm=SAC 
-python train.py --train --algorithm=PPO 
-python train.py --train --algorithm=DDPG 
+python train.py --train --algorithm=SAC --reward=euclidean
+python train.py --train --algorithm=PPO --reward=euclidean
+python train.py --train --algorithm=DDPG --reward=euclidean
+
+python train.py --train --algorithm=SAC --reward=euclidean_bar
+python train.py --train --algorithm=PPO --reward=euclidean_bar
+python train.py --train --algorithm=DDPG --reward=euclidean_bar
+
+python train.py --train --algorithm=SAC --reward=square
+python train.py --train --algorithm=PPO --reward=square
+python train.py --train --algorithm=DDPG --reward=square
+
+python train.py --train --algorithm=SAC --reward=square_bar
+python train.py --train --algorithm=PPO --reward=square_bar
+python train.py --train --algorithm=DDPG --reward=square_bar
